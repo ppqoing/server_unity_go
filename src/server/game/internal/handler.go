0fbe388a27ad942e929ac5e9571e2808dc86c05d
ppqoing <3170291724@qq.com>
2023-10-02 21:22:28 +0800

    vmware

--- src/server/game/internal/handler.go
+++ src/server/game/internal/handler.go
@@ -1,1 +1,23 @@
 package internal
+
+import (
+	"reflect"
+	"server/msg"
+
+	"github.com/name5566/leaf/gate"
+	"github.com/name5566/leaf/log"
+)
+
+func init() {
+
+}
+func handler(m interface{}, h interface{}) {
+	skeleton.RegisterChanRPC(reflect.TypeOf(m), h)
+
+}
+func handlerHello(args []interface{}) {
+	m := args[0].(*msg.Hello)
+	a := args[1].(gate.Agent)
+	log.Debug("hell0 %v %v", m.Name, m.Id)
+	a.WriteMsg(&msg.Hello{Name: "me", Id: 1})
+}
--- src/server/msg/msg.go
+++ src/server/msg/msg.go
@@ -1,11 +1,16 @@
 package msg
 
 import (
-	"github.com/name5566/leaf/network"
+	"github.com/name5566/leaf/network/json"
 )
 
-var Processor network.Processor
+var Processor = json.NewProcessor()
 
 func init() {
+	Processor.Register(&Hello{})
+}
 
+type Hello struct {
+	Name string
+	Id   int
 }
